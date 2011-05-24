92b5072fb768962c92366658031cecf54a56360c
hampe <hampe@mathematik.uni-kl.de>
2011-05-24 16:55:21 +0200

    * Changed all the tropical variety functions to use the ambient dimension fix on fans * Decided not to migrate the divisorByPLF function for now because of the frequent calls to intersection(Cone,Cone), which lies on the Perl side * Rewrote intersect_complete_fan

--- apps/tropical/src/divisor.cc
+++ apps/tropical/src/divisor.cc
@@ -38,157 +38,141 @@ namespace polymake { namespace tropical {
     //Documentation see header
     perl::Object intersect_complete_fan(perl::Object fan, perl::Object completeFan) {
       
-      Integer fanAmbientDim = fan.CallPolymakeMethod("ambient_dim_fix"); //FIXME: Replace both when possible
-      Integer compAmbientDim = completeFan.CallPolymakeMethod("ambient_dim_fix");
-      
-      //First check that ambient dimensions coincide
-      if(fanAmbientDim != compAmbientDim) {
-	dbgtrace << "Fan ambient dim is " << fanAmbientDim << endl;
-	dbgtrace << "Complete fan ambient dim is " << compAmbientDim << endl;
-	
+      //First we compute ambient dimensions and check that they are equal
+      int ambient_dim =  fan.CallPolymakeMethod("ambient_dim_fix"); //FIXME: Replace
+      int comp_ambient_dim = completeFan.CallPolymakeMethod("ambient_dim_fix"); //FIXME: Replace
+            
+      if(ambient_dim != comp_ambient_dim) {
 	throw std::runtime_error("Cannot intersect fans in different ambient dimension.");
       }
       
-      
       //If fan is zero-dimensional it is just a point, so it has no maximal cones.
       //But since no refinement is necessary anyway, we just return fan
-      //if(fan.give("DIM") == 0) return fan;
+      int dimension = fan.CallPolymakeMethod("dim_fix");
+      if(dimension == 0) return fan;
       
-      //Prepare containers for new values
-      Matrix<Rational> rays(0,fanAmbientDim);
-      Matrix<Rational> linealitySpace;
-	bool computedLineality = false;
-      Vector<Set<int> > maximalCones;
-      Set<Set<int> > maximalConeSet; //This is kept to check for doubles. It has the same content as maximalCones
-      Vector<int> tropicalWeights(0);
-      
-      //Read out old values
-      Matrix<Rational> fanOldrays = fan.give("RAYS");
-      Matrix<Rational> compOldrays = completeFan.give("RAYS");
-      Array<Integer> oldWeights;
+      //Extract values
+      Matrix<Rational> rays = fan.give("RAYS");
+      Matrix<Rational> lineality_space = fan.give("LINEALITY_SPACE");
+      Array<Integer> weights;
       bool weightsExist = false;
-	if(fan.exists("TROPICAL_WEIGHTS")) {
-	  oldWeights = fan.give("TROPICAL_WEIGHTS");
-	  weightsExist = true;
-	}
-      IncidenceMatrix<> fanMaximals = fan.give("MAXIMAL_CONES");
-      IncidenceMatrix<> compMaximals = completeFan.give("MAXIMAL_CONES");
-      Integer fanDimension = fan.CallPolymakeMethod("dim_fix"); //FIXME: Replace this when possible
-            
+      if(fan.exists("TROPICAL_WEIGHTS")) {
+	weights = fan.give("TROPICAL_WEIGHTS");
+	weightsExist = true;
+      }
+      IncidenceMatrix<> maximalCones = fan.give("MAXIMAL_CONES");
+      bool uses_homog = fan.give("USES_HOMOGENEOUS_C");
+      
+      Matrix<Rational> compRays = completeFan.give("RAYS");
+      IncidenceMatrix<> compMaximalCones = completeFan.give("MAXIMAL_CONES");
+      Matrix<Rational> compLinealitySpace = completeFan.give("LINEALITY_SPACE");
+      
+      //Result variables
+      Matrix<Rational> newRays(0,ambient_dim);
+      Vector<Set<int> > newCones;
+      Set<Set<int> > newConeSet;//This is kept to check for doubles. It has the same content as maximalCones
+      Matrix<Rational> newLineality(0,ambient_dim);
+	bool computedLineality = true;
+      Vector<Integer> newWeights;
+      
+      
       //If the complete fan is only a lineality space, it is the whole space, so we return fan
-      if(compMaximals.rows() == 0) {
+      if(compMaximalCones.rows() == 0) {
 	return fan;
       }
       
       //If the fan, however, is only a lineality space, we have to take care that it is considered
       // as a maximal cone of fan (note that at this point, the lineality dimension must then be > 0,
       // since the fan dimension is > 0
-      bool fanHasOnlyLineality = fanMaximals.rows() == 0;
-      int fanUpperBound = fanHasOnlyLineality? 1 : fanMaximals.rows();
-     
-      dbgtrace << "Starting to intersect maximal cone pairs" << endl;
+      bool fanHasOnlyLineality = maximalCones.rows() == 0;
+      int fanUpperBound = fanHasOnlyLineality? 1 : maximalCones.rows();
       
-      //Now intersect all pairs of maximal cones
-      for(int tindex = 0; tindex < fanUpperBound; tindex ++) {
-	for(int cindex = 0; cindex < compMaximals.rows(); cindex ++) {
-	  
-	  dbgtrace << "Intersecting cones " << tindex << " and " << cindex << endl;
-	  
-	  //Create the actual cones and compute the intersection
-	    perl::Object tcone("polytope::Cone<Rational>");
-	    perl::Object ccone("polytope::Cone<Rational>");
-	      tcone.take("RAYS") << (fanHasOnlyLineality? 
-				Matrix<Rational>(1,fanAmbientDim) : 
-				fanOldrays.minor(fanMaximals.row(tindex),All));
-				//Have to give it a ray to tell polymake about the ambient dimension
-	      ccone.take("RAYS") << compOldrays.minor(compMaximals.row(cindex),All);
-	      tcone.take("LINEALITY_SPACE") << fan.give("LINEALITY_SPACE");
-	      ccone.take("LINEALITY_SPACE") << completeFan.give("LINEALITY_SPACE");
-
-	    perl::Object inter = CallPolymakeFunction("intersection",tcone,ccone);
-	    Integer coneDimension = inter.give("CONE_DIM");
+      //Now intersect all pairs of maximal cones.
+      for(int fanIndex = 0; fanIndex < fanUpperBound; fanIndex++) {
+	for(int cIndex = 0; cIndex < compMaximalCones.rows(); cIndex++) {
+	    //Create the cones and compute the intersection
+	    perl::Object fanCone("polytope::Cone<Rational>");
+	    perl::Object compCone("polytope::Cone<Rational>");
+	      fanCone.take("RAYS") << (fanHasOnlyLineality?
+					Matrix<Rational>(1,ambient_dim) :
+					rays.minor(maximalCones.row(fanIndex),All));
+	      fanCone.take("LINEALITY_SPACE") << fan.give("LINEALITY_SPACE");
+	      compCone.take("RAYS") << compRays.minor(compMaximalCones.row(cIndex),All);
+	      compCone.take("LINEALITY_SPACE") << compLinealitySpace;
+	    perl::Object inter = CallPolymakeFunction("intersection",fanCone,compCone);
+	    int coneDimension = inter.give("CONE_DIM");
 	    
-	    dbgtrace << "Cone dimension is " << coneDimension << endl;
-	    
-	    //Check if they intersect in the correct dimension
-	    if(coneDimension == fanDimension) {
-		
-		dbgtrace << "Dimensions match. Checking rays" << endl;
-	      
-		//The first time we compute this, the lineality space of the intersection is copied as the lineality space of
+	    //Only consider intersections that have the correct dimension
+	    if(coneDimension == dimension) {
+	      //The first time we compute this, the lineality space of the intersection is copied as the lineality space of
 		//the resulting fan
 		if(!computedLineality) {
-		  inter.give("LINEALITY_SPACE") >> linealitySpace;
+		  inter.give("LINEALITY_SPACE") >> newLineality;
 		  computedLineality = true;
-		  
-		  dbgtrace << "Setting lineality space to " << linealitySpace << endl;
+		  dbgtrace << "Setting lineality space to " << newLineality << endl;
 		  
 		}
-			
-		//Now read off the rays and find the ones that have already been added to rays
-		Matrix<Rational> interRays = inter.give("RAYS");
 		
-		dbgtrace << "Ray matrix is " << interRays << endl;
+		//Now go through the rays of the intersection to find those already added
+		Matrix<Rational> interRays = inter.give("RAYS");
 		
 		Set<int> remainingRows;
 		Set<int> newMaximalCone;
-		//Go through all rays of the new cone
-		for(int row = 0; row < interRays.rows(); row++) {
+		for(int ray = 0; ray < interRays.rows(); ray++) {
 		  int raysIndex = -1;
 		  //Compare it to all existing rays
-		  for(int testRay = 0; testRay < rays.rows(); testRay ++) {
-		    if(rays.row(testRay) == interRays.row(row)) {
-			raysIndex = testRay;
-			break;
+		  for(int testRay = 0; testRay < newRays.rows(); testRay++) {
+		    if(interRays.row(ray) == newRays.row(testRay)) {
+		      raysIndex = testRay;
+		      break;
 		    }
 		  }
-		  //If we found one, add the index to the maximal cone, otherwise keep track of the row to create new rays later
+		  //If we found the ray, add the index to the maximal cone, otherwise
+		  //keep track of the row to create new rays later
 		  if(raysIndex >= 0) {
 		    newMaximalCone += raysIndex;
 		  }
 		  else {
-		    remainingRows += row;
+		    remainingRows += ray;
 		  }
-		}
+		}//END go through all intersection rays
 		
-		dbgtrace << "Remaining rows are " << remainingRows << ", Already existing are " << newMaximalCone << endl;
-		
- 		//Now add remaining rays and the corresponding cone (if it doesn't exist yet)
+		//Now add remaining rays and the corresponding cone (if it doesn't exist yet)
 		bool addCone = false;
-		if(remainingRows.size() > 0) addCone = true;
-		else {
-		  addCone = !(maximalConeSet.contains(newMaximalCone));
+		if(remainingRows.size() < 0) {
+		  addCone = true;
 		}
-		
-		dbgtrace << "Now we " << (addCone? "" : "don't ") << "add the cone." << endl;
+		else {
+		  addCone = !(newConeSet.contains(newMaximalCone));
+		}
 		
 		if(addCone) {
-		  
-		  dbgtrace << "Cone does not exist. Adding it" << endl;
-		  
-		  rays = rays / interRays.minor(remainingRows,All);
-		  for(int i = rays.rows() - remainingRows.size(); i < rays.rows(); i++) {
-		    newMaximalCone = newMaximalCone + i;
-		  }
-		  maximalCones = maximalCones | newMaximalCone;
-		  maximalConeSet = maximalConeSet + newMaximalCone;
-		  if(weightsExist) {
-		    tropicalWeights = tropicalWeights | oldWeights[tindex];
-		  }	      
+		    newRays = newRays / interRays.minor(remainingRows,All);
+		    for(int i = newRays.rows() - remainingRows.size(); i < newRays.rows(); i++) {
+		      newMaximalCone += i;
+		    }
 		}
-	    }
+		newCones = newCones | newMaximalCone;
+		newConeSet += newMaximalCone;
+		if(weightsExist) {
+		    newWeights = newWeights | weights[fanIndex];
+		}
+		
+	    }//End check intersection cone
 	    
-	}	
-      }//End iteration over all maximal cone pairs
-            
+	   
+	}//END iterate over complete fan cones
+      }//END iterate over fan cones
+      
       //Return result
       perl::Object result("fan::PolyhedralFan");
-	result.take("RAYS") << rays;
-	result.take("MAXIMAL_CONES") << maximalCones;
-	result.take("USES_HOMOGENEOUS_C") << fan.give("USES_HOMOGENEOUS_C");
-	result.take("LINEALITY_SPACE") << linealitySpace;
-	if(weightsExist) result.take("TROPICAL_WEIGHTS") << tropicalWeights;
+	result.take("RAYS") << newRays;
+	result.take("MAXIMAL_CONES") << newCones;
+	result.take("USES_HOMOGENEOUS_C") << uses_homog;
+	result.take("LINEALITY_SPACE") << newLineality;
+	if(weightsExist) result.take("TROPICAL_WEIGHTS") << newWeights;
       return result;
+      
       
     }
     
@@ -256,9 +240,6 @@ namespace polymake { namespace tropical {
       return result;
     }
     
-    perl::Object divisorByPLF(perl::Object fan, perl::Object) {
-      
-    }
 // ------------------------- PERL WRAPPERS ---------------------------------------------------
     
     UserFunction4perl("# @category Tropical geometry"
@@ -273,16 +254,6 @@ namespace polymake { namespace tropical {
     
     Function4perl(&divisorByValueVector,"divisorByValueVector(fan::PolyhedralFan, Vector<Rational>)");  
     
-    UserFunction4perl("# @category Tropical geometry"
-		      "# Computes the divisor of a PLFunction on a given tropical variety. "
-		      "# The result will be in homogeneous coordinates, whether the tropical variety uses them "
-		      "# or not. The function should be given on the affine coordinates of the variety, NOT the "
-		      "# homogeneous ones."
-		      "# @param fan::PolyhedralFan fan A tropical variety, on which the divisor is computed"
-		      "# @param PLFunction A function whose DOMAIN should be equal to the affine coordinate "
-		      "# space of the variety, i.e. AMBIENT_DIM-1, if the variety uses homogeneous coordinates, "
-		      "# AMBIENT_DIM otherwise. "
-		      "# @return The corresponding divisor as a tropical variety in homogeneous coordinates.",
-		      &divisorByPLF, "divisorByPLF(fan::PolyhedralFan, PLFunction)");
+
 }
 }
