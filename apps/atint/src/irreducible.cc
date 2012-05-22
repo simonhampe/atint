f35d6f3dd93f11616d63141b05703e5b31c18068
hampe <hampe@mathematik.uni-kl.de>
2012-05-22 11:15:50 +0200

    * Modified irreducibility computation: Instead of computing one absurdly huge linear equation system, I compute several small ones and intersect their solutions * Added a function to compute intersection of weight space with positive orthant as cone.

--- apps/atint/src/irreducible.cc
+++ apps/atint/src/irreducible.cc
@@ -39,15 +39,8 @@ namespace polymake { namespace atint {
   
   ///////////////////////////////////////////////////////////////////////////////////////
   
-  /**
-   @brief This function computes a system of linear equations that determine whether a given tropical cycle is irreducible.
-   @param perl::Object C A WeightedComplex object that is assumed to be balanced
-   @param Map<int,int> A reference that will be set to a map describing how to translate an element in the kernel of this matrix. More precisely: For maximal cone i map(i) describes the index of the solution (corresponding to a column index of the result of this function) where you should look for the weight of the cone. I.e. if you assign to all cones the weight that you find in an integer solution at the positions described by this map, you obtain a balanced cycle.
-   @return Matrix<Integer> A system of linear equations: For each codimension one cell t it contains a block describing the balancing condition around this cell (i.e. of the form (u_1,... u_k,v_1,..v_l), where the u_i are the lattice normal vectors at t and v_1,...v_l is a lattice basis for t. Additionally the matrix contains equations to make sure that a single maximal cone is assigned the same weight in each of the above systems
-   */
-  Matrix<Integer> cycle_irreducibility_system(perl::Object C, Map<int,int> &index_translator) {
-    
-    //dbgtrace << "Extracting values..." << endl;
+  //Documentation see perl wrapper
+  Matrix<Integer> cycle_weight_system(perl::Object C) {
     
     //Extract values
     int ambient_dim = C.give("CMPLX_AMBIENT_DIM");
@@ -61,27 +54,13 @@ namespace polymake { namespace atint {
     Map<int,Map<int, Vector<Integer> > > lattice_normals = C.give("LATTICE_NORMALS");
     bool uses_homog = C.give("USES_HOMOGENEOUS_C");
     
-    //Compute future dimension of (the upper part of ) the matrix
-    int f_rows = codim_1_faces.rows() * ambient_dim;
-    int f_cols = codim_1_faces.rows() * (dim-1);
-    for(int s = 0; s < codim_1_faces.rows(); s++) {
-      f_cols += codim_in_maximal.row(s).size();
-    }
+    //Prepare matrix
+    Matrix<Rational> result(0, maximal_cones.rows());
     
-    Matrix<Integer> F(f_rows,f_cols);
-    
-    Vector<bool> cone_translated(maximal_cones.rows()); //Whether the column index for cone i is computed
-    index_translator = Map<int,int>();
-    
-    //Generate balancing equation blocks
-    int rowindex = 0;
-    int colindex = 0;
-    Map<int,Map<int,int> > normal_col_index; //Saves for each normal vector its column index
+    //Iterate all codimension one cells
     for(int tau = 0; tau < codim_1_faces.rows(); tau++) {
-      //dbgtrace << "Computing equation block for codim one cell " << tau << endl;
-      normal_col_index[tau] = Map<int,int>();
-      
-      Matrix<Integer> M_tau(ambient_dim,0);
+      //Create the linear equation system for the balancing condition at tau
+      Matrix<Rational> M_tau(ambient_dim,0);
       
       //dbgtrace << "Appending normal vectors " << endl;
       
@@ -91,59 +70,155 @@ namespace polymake { namespace atint {
 	  Vector<Integer> lnormal = ((lattice_normals[tau])[*sigma]);
 	    if(uses_homog) lnormal = lnormal.slice(~scalar2set(0));
 	  M_tau |= lnormal;
-	  (normal_col_index[tau])[*sigma] = colindex + M_tau.cols()-1;
-	  if(!cone_translated[*sigma]) {
-	      cone_translated[*sigma] = true;
-	      index_translator[*sigma] = (normal_col_index[tau])[*sigma];
-	  }
-      }
-      
-      //dbgtrace << "Appending lattice basis" << endl;
+      }//END add normal vectors
       
       //Compute a lattice basis for this cell
       if(dim > 1) {
 	Matrix<Integer> lbasis = latticeBasisFromRays(rays.minor(codim_1_faces.row(tau),All),lineality,uses_homog);
 	  if(uses_homog) lbasis = lbasis.minor(All,~scalar2set(0));
 	M_tau |= T(lbasis);
-      }
+      }//END compute lattice basis
       
-      //dbgtrace << "Inserting equation block " <<  M_tau << endl;
+      //Now compute the kernel of this system
+      Matrix<Rational> K_tau = null_space(M_tau);
       
-      //Insert M_tau
-      F.minor(sequence(rowindex,M_tau.rows()), sequence(colindex, M_tau.cols())) = M_tau;
-      rowindex += M_tau.rows();
-      colindex += M_tau.cols();
+      //dbgtrace << "Kernel of matrix is " << K_tau << endl;
       
-    }//END generate balancing equation blocks
+      //Compute the projection to the weight space:
+      Matrix<Rational> P_tau(K_tau.rows(),maximal_cones.rows());
+      P_tau.minor(All,adjacent_maximal) = K_tau.minor(All, sequence(0, maximal_cones.rows()));
+      //The remaining cones can have arbitrary weights, so we append the appropriate unit vectors
+      P_tau /= (unit_matrix<Rational>(maximal_cones.rows()).minor(sequence(0,maximal_cones.rows()) - adjacent_maximal,All));
+      
+      
+      //dbgtrace << "Projected to " << P_tau << endl;
+      
+      //Now we compute the equations in the weight space,add them to the current equations
+      //and reduce
+      result /= null_space(P_tau);
+      result = result.minor(basis_rows(result),All);
+      
+    }//END iterate codimension one cells
     
+    return makeInteger(result);
     
-    //Now generate maximal cone glue equations
-    for(int sigma = 0; sigma < maximal_cones.rows(); sigma++) {
-	//dbgtrace << "Computing glueing equations for maximal cone " << sigma << endl;
-	Vector<int> faces(maximal_to_codim.row(sigma));
-	if(faces.dim() <= 1) continue;
-	//For all pairs of codim one cells in sigma, make sure the corresponding normal vectors
-	//are assigned the same weight, i.e. the corresponding entries in the solution must be the same
-	//More precisely, we just have to demand that the solution for the system of the "first" codimension
-	//one cell of sigma is the same as the solution for any other system
-	Matrix<Integer> N_sigma(0,f_cols);
-	
-	for(int tau = 1; tau < faces.dim(); tau++) {
-	  N_sigma /= ( unit_vector<Integer>(f_cols, (normal_col_index[faces[0]])[sigma]) - 
-			 unit_vector<Integer>(f_cols, (normal_col_index[faces[tau]])[sigma]));
-	}
-	
-	
-	F /= N_sigma;
-    }//END generate maximal cone glue equations
-    
-    return F;
-  }
+  }//END cycle_weight_system
+  
+  ///////////////////////////////////////////////////////////////////////////////////////
+  
+//   /**
+//    @brief This function computes a system of linear equations that determine whether a given tropical cycle is irreducible.
+//    @param perl::Object C A WeightedComplex object that is assumed to be balanced
+//    @param Map<int,int> A reference that will be set to a map describing how to translate an element in the kernel of this matrix. More precisely: For maximal cone i map(i) describes the index of the solution (corresponding to a column index of the result of this function) where you should look for the weight of the cone. I.e. if you assign to all cones the weight that you find in an integer solution at the positions described by this map, you obtain a balanced cycle.
+//    @return Matrix<Integer> A system of linear equations: For each codimension one cell t it contains a block describing the balancing condition around this cell (i.e. of the form (u_1,... u_k,v_1,..v_l), where the u_i are the lattice normal vectors at t and v_1,...v_l is a lattice basis for t. Additionally the matrix contains equations to make sure that a single maximal cone is assigned the same weight in each of the above systems
+//    */
+//   Matrix<Integer> cycle_irreducibility_system(perl::Object C, Map<int,int> &index_translator) {
+//     
+//     //dbgtrace << "Extracting values..." << endl;
+//     
+//     //Extract values
+//     int ambient_dim = C.give("CMPLX_AMBIENT_DIM");
+//     int dim = C.give("CMPLX_DIM");
+//     Matrix<Rational> rays = C.give("RAYS");
+//     Matrix<Rational> lineality = C.give("LINEALITY_SPACE");
+//     IncidenceMatrix<> maximal_cones = C.give("MAXIMAL_CONES");
+//     IncidenceMatrix<> codim_1_faces = C.give("CODIM_1_FACES");
+//     IncidenceMatrix<> codim_in_maximal = C.give("CODIM_1_IN_MAXIMAL_CONES");
+//     IncidenceMatrix<> maximal_to_codim = T(codim_in_maximal);
+//     Map<int,Map<int, Vector<Integer> > > lattice_normals = C.give("LATTICE_NORMALS");
+//     bool uses_homog = C.give("USES_HOMOGENEOUS_C");
+//     
+//     //Compute future dimension of (the upper part of ) the matrix
+//     int f_rows = codim_1_faces.rows() * ambient_dim;
+//     int f_cols = codim_1_faces.rows() * (dim-1);
+//     for(int s = 0; s < codim_1_faces.rows(); s++) {
+//       f_cols += codim_in_maximal.row(s).size();
+//     }
+//     
+//     Matrix<Integer> F(f_rows,f_cols);
+//     
+//     Vector<bool> cone_translated(maximal_cones.rows()); //Whether the column index for cone i is computed
+//     index_translator = Map<int,int>();
+//     
+//     //Generate balancing equation blocks
+//     int rowindex = 0;
+//     int colindex = 0;
+//     Map<int,Map<int,int> > normal_col_index; //Saves for each normal vector its column index
+//     for(int tau = 0; tau < codim_1_faces.rows(); tau++) {
+//       //dbgtrace << "Computing equation block for codim one cell " << tau << endl;
+//       normal_col_index[tau] = Map<int,int>();
+//       
+//       Matrix<Integer> M_tau(ambient_dim,0);
+//       
+//       //dbgtrace << "Appending normal vectors " << endl;
+//       
+//       //Add all normal vectors
+//       Set<int> adjacent_maximal = codim_in_maximal.row(tau);
+//       for(Entire<Set<int> >::iterator sigma = entire(adjacent_maximal); !sigma.at_end(); sigma++) {
+// 	  Vector<Integer> lnormal = ((lattice_normals[tau])[*sigma]);
+// 	    if(uses_homog) lnormal = lnormal.slice(~scalar2set(0));
+// 	  M_tau |= lnormal;
+// 	  (normal_col_index[tau])[*sigma] = colindex + M_tau.cols()-1;
+// 	  if(!cone_translated[*sigma]) {
+// 	      cone_translated[*sigma] = true;
+// 	      index_translator[*sigma] = (normal_col_index[tau])[*sigma];
+// 	  }
+//       }
+//       
+//       //dbgtrace << "Appending lattice basis" << endl;
+//       
+//       //Compute a lattice basis for this cell
+//       if(dim > 1) {
+// 	Matrix<Integer> lbasis = latticeBasisFromRays(rays.minor(codim_1_faces.row(tau),All),lineality,uses_homog);
+// 	  if(uses_homog) lbasis = lbasis.minor(All,~scalar2set(0));
+// 	M_tau |= T(lbasis);
+//       }
+//       
+//       //dbgtrace << "Inserting equation block " <<  M_tau << endl;
+//       
+//       //Insert M_tau
+//       F.minor(sequence(rowindex,M_tau.rows()), sequence(colindex, M_tau.cols())) = M_tau;
+//       rowindex += M_tau.rows();
+//       colindex += M_tau.cols();
+//       
+//     }//END generate balancing equation blocks
+//     
+//     
+//     //Now generate maximal cone glue equations
+//     for(int sigma = 0; sigma < maximal_cones.rows(); sigma++) {
+// 	//dbgtrace << "Computing glueing equations for maximal cone " << sigma << endl;
+// 	Vector<int> faces(maximal_to_codim.row(sigma));
+// 	if(faces.dim() <= 1) continue;
+// 	//For all pairs of codim one cells in sigma, make sure the corresponding normal vectors
+// 	//are assigned the same weight, i.e. the corresponding entries in the solution must be the same
+// 	//More precisely, we just have to demand that the solution for the system of the "first" codimension
+// 	//one cell of sigma is the same as the solution for any other system
+// 	Matrix<Integer> N_sigma(0,f_cols);
+// 	
+// 	for(int tau = 1; tau < faces.dim(); tau++) {
+// 	  N_sigma /= ( unit_vector<Integer>(f_cols, (normal_col_index[faces[0]])[sigma]) - 
+// 			 unit_vector<Integer>(f_cols, (normal_col_index[faces[tau]])[sigma]));
+// 	}
+// 	
+// 	
+// 	F /= N_sigma;
+//     }//END generate maximal cone glue equations
+//     
+//     return F;
+//   }//END cycle_irreducibility_system
   
   ///////////////////////////////////////////////////////////////////////////////////////
   
   //Documentation see perl wrapper
   Matrix<Integer> cycle_weight_space(perl::Object C) {
+    
+    //First we compute the weight system
+    Matrix<Integer> weight_system = cycle_weight_system(C);
+    //The weight solutions are the kernel...
+    Matrix<Rational> ker(null_space(weight_system));
+    //... but we have to make them a lattice basis
+    return latticeBasisFromRays(ker, Matrix<Rational>(0, ker.cols()), false);
+    /*
     
     IncidenceMatrix<> maximal_cones = C.give("MAXIMAL_CONES");
     
@@ -164,7 +239,27 @@ namespace polymake { namespace atint {
     }
     
     //Now compute a lattice basis
-    return latticeBasisFromRays(solutions, Matrix<Rational>(0,solutions.cols()), false);
+    return latticeBasisFromRays(solutions, Matrix<Rational>(0,solutions.cols()), false);*/
+    
+  }
+  
+  ///////////////////////////////////////////////////////////////////////////////////////
+  
+  //Documentation see perl wrapper
+  perl::Object cycle_weight_cone(perl::Object C) {
+    
+    //First we compute the weight system
+    Matrix<Rational> wsys (cycle_weight_system(C));
+    
+    //Create inequalities of the positive orthant
+    Matrix<Rational> positive = unit_matrix<Rational>(wsys.cols());
+    
+    //Now create the cone
+    perl::Object c("polytope::Cone");
+      c.take("INEQUALITIES") << positive;
+      c.take("EQUATIONS") << wsys;
+      
+    return c;
     
   }
   
@@ -184,7 +279,7 @@ namespace polymake { namespace atint {
     }
     
     Map<int,int> dummy;
-    Matrix<Integer> F = cycle_irreducibility_system(C, dummy);
+    Matrix<Integer> F = cycle_weight_system(C);
     return rank(null_space(F)) == 1;
   }
   
@@ -204,6 +299,18 @@ namespace polymake { namespace atint {
 		    "# @param WeightedComplex C A tropical cycle"
 		    "# @return Matrix<Integer> A Z-basis of the weight space (given as row vectors)",
 		    &cycle_weight_space, "cycle_weight_space(WeightedComplex)");
-	
+
+  UserFunction4perl("# @category Tropical geometry / Irreducibility"
+		    "# This function computes for a given tropical cycle C a representation of "
+		    "# cycle_weight_space(C) in terms of linear equations. More precisely, this computes"
+		    "# a matrix, whose integer kernel is cycle_weight_space(C)",
+		    &cycle_weight_system, "cycle_weight_system(WeightedComplex)");
+  
+  UserFunction4perl("# @category Tropical geometry / Irreducibility"
+		    "# This function computes the intersection of the weight space of a cycle (as given by "
+		    "# cycle_weight_space) with the positive orthant."
+		    "# @param WeightedComplex C"
+		    "# @return polytope::Cone The positive part of the weight space as a (rational) cone", 
+		    &cycle_weight_cone, "cycle_weight_cone(WeightedComplex)");
   
 }}
