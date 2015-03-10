fbb23ed6758fa0afed5dff4faf74e3c6cb12d1ab
hampe <devnull@localhost>
2015-03-10 15:19:24 +0100

    * Added requirement for bundled cdd. No longer compatible with polymake < 2.14, I guess.

--- apps/atint/src/perl/wrap-basicoperations.cc
+++ apps/atint/src/perl/wrap-basicoperations.cc
@@ -1,5 +1,5 @@
-/* Copyright (c) 1997-2013
-   Ewgenij Gawrilow, Michael Joswig (Technische Universitaet Darmstadt, Germany)
+/* Copyright (c) 1997-2015
+   Ewgenij Gawrilow, Michael Joswig (Technische Universitaet Berlin, Germany)
    http://www.polymake.org
 
    This program is free software; you can redistribute it and/or modify it
@@ -11,6 +11,7 @@
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
+--------------------------------------------------------------------------------
 */
 
 namespace polymake { namespace atint { namespace {
