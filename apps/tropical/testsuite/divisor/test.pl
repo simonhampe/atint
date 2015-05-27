298d69f36fcedf574009219765624d8b46cc1621
Simon Hampe <simon.hampe@googlemail.com>
2015-05-27 14:45:02 +0200

    Tests and fixes

--- apps/tropical/testsuite/divisor/test.pl
+++ apps/tropical/testsuite/divisor/test.pl
@@ -1,3 +1,5 @@
 compare_object( "div1", divisor(load("x1"), load("r1")))
 	and
-compare_object( "div2", divisor(load("x2"), load("r2")));
+compare_object( "div2", divisor(load("x2"), load("r2")))
+	and
+compare_object( "div3", divisor(load("x3"), load("r3")));
